db65826e40040e4d2e35752ef24d9eaff9a9d74f
Nitin Singh <nitinsinghofficial9@gmail.com>
2021-12-29 19:52:41 +0530

    synchronous nature of js

---
+++ JS/AJAX-with-JS/sync_nature_of_js.js
@@ -0,0 +1,17 @@
+let x = 10;
+console.log("start");
+if(x>=5) {
+  for(let i=0;i<100000000;i++) {
+    // some code
+  }
+  //network call -> make my trip
+} else {
+  for(let i=0;i<10000000;i++) {
+    // some more code
+  }
+}
+
+/*
+start
+hey there
+*/
