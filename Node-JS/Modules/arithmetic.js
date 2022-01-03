1f7d1850c8eb1c0fc6acdfcbb6d8d3cf1b769825
Nitin Singh <nitinsinghofficial9@gmail.com>
2022-01-03 22:34:02 +0530

    modules

---
+++ Node-JS/Modules/arithmetic.js
@@ -0,0 +1,13 @@
+const add = function (num1,num2) {
+    return num1 + num2
+}
+
+const sub = function (num1,num2) {
+    return num1 - num2
+}
+
+const mul = function (num1,num2) {
+    return num1 * num2
+}
+
+module.exports = {addtion: add, subtraction: sub, multiplication: mul};
---
+++ Node-JS/Modules/index.js
@@ -0,0 +1,10 @@
+const action = require('./add');
+const obj = require('./arithmetic');
+
+const {add,sub,mul} = require('./arithmetic')
+
+//require is also a global
+
+console.log(action(10,12));
+console.log(obj);
+console.log(obj.sub(9,10));
